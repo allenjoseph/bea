632049892546a43696763e324cad97f694b4a360
AllenJoseph <allen.joseph.v.a@gmail.com>
2014-11-18 22:06:34 -0500

    add foundatation

--- bea/forms.py
+++ bea/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 class LoginForm(forms.Form):
     usuario = forms.CharField(
-                widget=forms.TextInput(attrs={'class' : 'form-control'}),
+                widget=forms.TextInput(attrs={'class' : ''}),
                 error_messages={'required': 'Ingresa tu usuario'}
             )
     password = forms.CharField(
-                widget=forms.PasswordInput(attrs={'class' : 'form-control'}),
+                widget=forms.PasswordInput(attrs={'class' : ''}),
                 error_messages={'required': 'Ingresa tu password'}
             )
         
